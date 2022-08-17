608620968d7a540d05e6b0ae4b0b8fcd52d6acd2
Brian Sam-Bodden <bsb@redis.com>
2022-08-17 10:17:28 -0700

    Step 3: Implement autocomplete API endpoint in AirportsController

--- src/main/java/com/redis/om/autocomplete/controllers/AirportsController.java
+++ src/main/java/com/redis/om/autocomplete/controllers/AirportsController.java
@@ -6,9 +6,19 @@ import static org.springframework.web.bind.annotation.RequestMethod.OPTIONS;
 import static org.springframework.web.bind.annotation.RequestMethod.PATCH;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.redis.om.autocomplete.repository.AirportsRepository;
+import com.redis.om.spring.repository.query.autocomplete.AutoCompleteOptions;
+
+import io.redisearch.Suggestion;
 
 @CrossOrigin( //
     methods = { POST, GET, OPTIONS, DELETE, PATCH }, //
@@ -21,4 +31,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/airports")
 public class AirportsController {
+
+  @Autowired
+  private AirportsRepository repository;
+
+  @GetMapping("/search/{q}")
+  public List<Suggestion> query(@PathVariable("q") String query) {
+    List<Suggestion> suggestions = repository //
+        .autoCompleteName(query, AutoCompleteOptions.get().withPayload());
+    return suggestions;
+  }
+
 }
